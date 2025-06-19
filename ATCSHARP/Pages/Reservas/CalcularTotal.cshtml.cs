f6137b8e3d72ec6e085eb1991559f7895d863b61
Taylan <taylansilva0402@gmail.com>
2025-06-19 15:23:36 -0300

    Criando a pagina de simulação do valor total de uma reserva com o Entity Framework.

---
+++ ATCSHARP/Pages/Reservas/CalcularTotal.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ATCSHARP.Data;
+using ATCSHARP.Models;
+
+namespace ATCSHARP.Pages.Reservas
+{
+    public class CalcularTotalModel : PageModel
+    {
+        private readonly ATCSHARP.Data.ApplicationDbContext _context;
+
+        public CalcularTotalModel(ATCSHARP.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id");
+        ViewData["PacoteTuristicoId"] = new SelectList(_context.Pacotes, "Id", "Id");
+            return Page();
+        }
+
+        [BindProperty]
+        public Reserva Reserva { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Reservas.Add(Reserva);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
